122cd2c8e762f643027d2e8be2ec3534790c9a38
Patrick Härtl <haertl@ux3d.io>
2022-01-28 15:04:42 +0100

    Build libressl static

--- triplets/x64-linux.cmake
+++ triplets/x64-linux.cmake
@@ -3,4 +3,6 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
-set(VCPKG_ENV_PASSTHROUGH PATH)
+if(PORT MATCHES "libressl")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
